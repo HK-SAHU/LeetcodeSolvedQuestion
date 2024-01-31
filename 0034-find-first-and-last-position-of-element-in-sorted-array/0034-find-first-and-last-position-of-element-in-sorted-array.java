6b9b8b701d7238baa82f2bafc010fab50f186459
Harsh Kumar Sahu <135067768+HK-SAHU@users.noreply.github.com>
2024-01-31 10:00:53 +0530

    Time: 1 ms (22.61%), Space: 45.9 MB (28.15%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,8 +1,8 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int[] ans= new int[2];
-        ans[0]= assignLast(nums, target);
-        ans[1]=assignFirst(nums, target);
+        ans[0]= assignFirst(nums, target);
+        ans[1]=assignLast(nums, target);
         return ans;
     }
     
@@ -12,6 +12,7 @@ class Solution {
         for(int i=0; i<n;i++){
             if(nums[i]==target){
                 found=i;
+                break;
             }
         }
         return found;
@@ -23,6 +24,7 @@ class Solution {
         for(int i=n-1; i>=0;i--){
             if(nums[i]==target){
                 found=i;
+                break;
             }
         }
         return found;
