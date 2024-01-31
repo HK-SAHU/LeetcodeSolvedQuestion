6b0ee5c7bdcb57fa876cf44c597caa6715c72ae4
Harsh Kumar Sahu <135067768+HK-SAHU@users.noreply.github.com>
2024-01-31 10:06:01 +0530

    Time: 0 ms (100.00%), Space: 45.7 MB (63.14%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,5 +1,65 @@
 class Solution {
+    
     public int[] searchRange(int[] nums, int target) {
+
+        int[] result = new int[2];
+        result[0] = searchFirst(nums, target);
+        result[1] = searchLast(nums, target);
+
+        return result;
+    }
+
+  
+    public int searchFirst(int[] nums, int target){
+        int left = 0; 
+        int right = nums.length - 1; 
+        int index = -1; 
+
+        while(left <= right){
+
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] == target){
+                index = mid; 
+                right = mid - 1; 
+            }
+
+            else if(nums[mid] < target){
+                left = mid + 1; 
+            }
+            else{
+                right = mid - 1; 
+            }
+        }
+
+        return index;
+    }
+
+    public int searchLast(int[] nums, int target){
+        int left = 0; 
+        int right = nums.length - 1; 
+        int index = -1; 
+
+        while(left <= right){
+
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] == target){
+                index = mid; 
+                left = mid + 1;
+            }
+
+            else if(nums[mid] < target){
+                left = mid + 1; 
+            }
+            else{
+                right = mid - 1;
+            }
+        }
+
+        return index;
+    }
+    /*public int[] searchRange(int[] nums, int target) {
         int[] ans= new int[2];
         ans[0]= assignFirst(nums, target);
         ans[1]=assignLast(nums, target);
@@ -28,7 +88,7 @@ class Solution {
             }
         }
         return found;
-    }
+    }*/
     
     
 }
