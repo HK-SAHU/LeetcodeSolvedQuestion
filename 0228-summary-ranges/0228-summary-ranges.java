008f2b8d20714a4d0579577f1ece0cc66efb40dc
Harsh Kumar Sahu <135067768+HK-SAHU@users.noreply.github.com>
2024-02-16 23:07:51 +0530

    Time: 5 ms (83.80%), Space: 41.8 MB (37.43%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<String> summaryRanges(int[] nums) {
-        /*List<String> list=new ArrayList();
+        List<String> list=new ArrayList();
         if(nums.length==1){
             list.add(nums[0]+"");
             return list;
@@ -16,21 +16,6 @@ class Solution {
                 list.add(a+"");
             }
         }
-        return list;*/
-        
-        List<String> ans = new ArrayList<>();
-
-    for (int i = 0; i < nums.length; ++i) {
-      final int begin = nums[i];
-      while (i + 1 < nums.length && nums[i] == nums[i + 1] - 1)
-        ++i;
-      final int end = nums[i];
-      if (begin == end)
-        ans.add("" + begin);
-      else
-        ans.add("" + begin + "->" + end);
-    }
-
-    return ans;
+        return list;
     }
 }
