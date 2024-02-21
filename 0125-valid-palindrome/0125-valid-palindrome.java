68826d8a555cba4665c4489b4f6922f08ebb18dc
Harsh Kumar Sahu <135067768+HK-SAHU@users.noreply.github.com>
2024-02-21 13:05:29 +0530

    Time: 3 ms (76.30%), Space: 42.6 MB (95.08%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,22 +1,48 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        boolean yes=false;
+        // boolean yes=false;
+        // s=s.toLowerCase();
+        // s=s.trim();
+        // s=s.replaceAll("[^a-z0-9]","");
+        // int n=s.length();
+        // int start=0;
+        // int end=n-1;
+        // while(start<=end){
+        //     if(s.charAt(start)==s.charAt(end)){
+        //         start++;
+        //         end--;
+        //     } 
+        //     else{
+        //         return yes;
+        //     }
+        // }
+        // return !yes;
+        
+        
         s=s.toLowerCase();
-        s=s.trim();
-        s=s.replaceAll("[^a-z0-9]","");
-        int n=s.length();
+        int n= s.length();
         int start=0;
         int end=n-1;
         while(start<=end){
-            if(s.charAt(start)==s.charAt(end)){
+            if(!isAlphanumeric(s.charAt(start))){
+                start++;
+            }
+            else if(!isAlphanumeric(s.charAt(end))){
+                end--;
+            }
+            else if(s.charAt(start)!=s.charAt(end)){
+                return false;
+            }
+            else{
                 start++;
                 end--;
-            } 
-            else{
-                return yes;
             }
         }
-        return !yes;
+        return true;
         
+    }
+    
+    static boolean isAlphanumeric(char c) {
+        return Character.isLetter(c) || Character.isDigit(c);
     }
 }
