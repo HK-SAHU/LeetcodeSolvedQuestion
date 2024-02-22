3921ceadda3a8111be4afd6ef1aaa8c081d3caab
Harsh Kumar Sahu <135067768+HK-SAHU@users.noreply.github.com>
2024-02-22 14:46:26 +0530

    Time: 1 ms (87.45%), Space: 40.5 MB (86.11%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,20 +1,43 @@
 class Solution {
     public int mySqrt(int x) {
+//         if (x == 0 || x == 1)
+//             return x;
+
+//         int start = 1, end = x, ans = 0;
+//         while (start <= end) {
+//             int mid = start + (end - start) / 2;
+
+//             // Check if mid is the square root
+//             if (mid <= x / mid) {
+//                 start = mid + 1;
+//                 ans = mid;
+//             } else {
+//                 end = mid - 1;
+//             }
+//         }
+//         return ans;
+        
         if (x == 0 || x == 1)
             return x;
-
-        int start = 1, end = x, ans = 0;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            // Check if mid is the square root
-            if (mid <= x / mid) {
-                start = mid + 1;
-                ans = mid;
-            } else {
-                end = mid - 1;
+        int start=1;
+        int end=x;
+        int result=0;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            long val=(long)mid*mid;
+            
+            if(val==x){
+                return mid;
+            }
+            else if(val<x){
+                result=mid;
+                start=mid+1;
+            }
+            else{
+                end= mid-1;
+                
             }
         }
-        return ans;
+        return result;
     }
 }
