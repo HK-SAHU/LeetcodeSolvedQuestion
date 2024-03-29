96f782c30c6fff56223a8fe59f2f7dab6209868e
Harsh Kumar Sahu <135067768+HK-SAHU@users.noreply.github.com>
2024-03-30 01:50:22 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (71.43%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,6 +10,45 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//         if(list1==null && list2==null){
+//             return null;
+//         }
+//         if(list1==null){
+//             return list2;
+//         }
+//         if(list2==null){
+//             return list1;
+//         }
+        
+//         ListNode temp1=list1;
+//         ListNode temp2=list2;
+//         ListNode head = new ListNode(100);
+//         ListNode temp=head;
+//         while(temp1!=null && temp2!=null){
+//             if(temp1.val<=temp2.val){
+//                 ListNode a= new ListNode(temp1.val);
+//                 temp.next=a;
+//                 temp=a;
+//                 temp1= temp1.next;
+//             }
+//             else{
+//                 ListNode b= new ListNode(temp2.val);
+//                 temp.next=b;
+//                 temp=b;
+//                 temp2= temp2.next;
+//             }
+//         }
+        
+//         if(temp1==null){
+//             temp.next=temp2;
+//         }
+//         else{
+//             temp.next= temp1;
+//         }
+//         return head.next;
+         
+        
+        ///////////////////////// without using extra space //////////////////////////
         if(list1==null && list2==null){
             return null;
         }
@@ -20,32 +59,29 @@ class Solution {
             return list1;
         }
         
-        ListNode temp1=list1;
-        ListNode temp2=list2;
-        ListNode head = new ListNode(100);
-        ListNode temp=head;
+        ListNode temp1 = list1;
+        ListNode temp2 = list2;
+        ListNode head= new ListNode(100);
+        ListNode temp = head;
+        
         while(temp1!=null && temp2!=null){
             if(temp1.val<=temp2.val){
-                ListNode a= new ListNode(temp1.val);
-                temp.next=a;
-                temp=a;
-                temp1= temp1.next;
+                temp.next=temp1;
+                temp=temp1;
+                temp1=temp1.next;
             }
             else{
-                ListNode b= new ListNode(temp2.val);
-                temp.next=b;
-                temp=b;
+                temp.next=temp2;
+                temp=temp2;
                 temp2= temp2.next;
             }
         }
-        
         if(temp1==null){
             temp.next=temp2;
         }
         else{
-            temp.next= temp1;
+            temp.next=temp1;
         }
         return head.next;
-         
     }
 }
