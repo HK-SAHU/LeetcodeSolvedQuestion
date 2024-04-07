03962086c47fced92d2d215c74ef3b2b3bcee872
Harsh Kumar Sahu <135067768+HK-SAHU@users.noreply.github.com>
2024-04-07 23:53:30 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (18.22%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -11,25 +11,20 @@
 class Solution {
     public ListNode swapPairs(ListNode head) {
         if (head == null || head.next == null) return head;
-
         ListNode dummy = new ListNode(0);
         dummy.next = head;
         ListNode prev = dummy;
-
         while (head != null && head.next != null) {
             ListNode first = head;
             ListNode second = head.next;
-
             // Swapping
             prev.next = second;
             first.next = second.next;
             second.next = first;
-
             // Move pointers forward
             prev = first;
             head = first.next;
         }
-
         return dummy.next;
     }
 
