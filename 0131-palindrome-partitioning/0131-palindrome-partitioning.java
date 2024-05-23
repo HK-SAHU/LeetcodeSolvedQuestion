1a36283348e53ae3df950fc5408308fb7e00bd3e
Harsh Kumar Sahu <135067768+HK-SAHU@users.noreply.github.com>
2024-05-24 01:41:37 +0530

    Time: 7 ms (94.96%), Space: 56.8 MB (45.73%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -4,13 +4,11 @@ class Solution {
     dfs(s, 0, new ArrayList<>(), ans);
     return ans;
   }
-
-  private void dfs(final String s, int start, List<String> path, List<List<String>> ans) {
+  public void dfs(final String s, int start, List<String> path, List<List<String>> ans) {
     if (start == s.length()) {
       ans.add(new ArrayList<>(path));
       return;
     }
-
     for (int i = start; i < s.length(); ++i)
       if (isPalindrome(s, start, i)) {
         path.add(s.substring(start, i + 1));
@@ -18,8 +16,7 @@ class Solution {
         path.remove(path.size() - 1);
       }
   }
-
-  private boolean isPalindrome(final String s, int l, int r) {
+  public boolean isPalindrome(final String s, int l, int r) {
     while (l < r)
       if (s.charAt(l++) != s.charAt(r--))
         return false;
