58e60e968a7c2f973379b2b36c4d6b4712894c8a
Harsh Kumar Sahu <135067768+HK-SAHU@users.noreply.github.com>
2024-06-03 17:09:58 +0530

    Time: 3 ms (42.09%), Space: 7.6 MB (59.97%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -3,14 +3,18 @@ public:
     
     bool isValid(string s) {
         stack<char> st;
-        for(int i=0; i<s.length();i++){
+        for(int i=0;i<s.length();i++){
             char ch= s[i];
+            
             if(ch=='(' || ch=='{' || ch=='[') st.push(ch);
-            else {
+            
+            else{
                 if(!st.empty()){
                     char top= st.top();
-                    if((ch==')' && top=='(') || (ch=='}' && top=='{')|| (ch==']' && top=='[') ) st.pop();
-                    else return false;
+                    if ((ch==')' && top=='(') || (ch=='}' && top=='{') || (ch==']' && top=='[')) st.pop();
+                    else{
+                        return false;
+                    }
                 }
                 else{
                     return false;
