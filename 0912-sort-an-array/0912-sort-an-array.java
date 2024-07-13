f502003bbf7b02f775a88951b2c30aad3b462e9b
Harsh Kumar Sahu <135067768+HK-SAHU@users.noreply.github.com>
2024-07-13 16:57:04 +0530

    Time: 27 ms (58.19%), Space: 56.1 MB (52.84%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -0,0 +1,62 @@
+class Solution {
+    public static void merge(int[] nums, int start, int mid, int end){
+        //i= leftsubbarray pointer, j= right subarray poinnter, k= original subarray pointer
+        int n1= mid- start +1;
+        int n2=end-mid;
+        
+        int[] left= new int[n1];
+        int[] right= new int[n2];
+        
+        // copy data to temp arrays
+        for(int i=0;i<n1;i++){
+            left[i]= nums[start+i];
+        }
+        
+        for(int j=0;j<n2;j++){
+            right[j]=nums[mid+1+j];
+        }
+        
+        int i=0;
+        int j=0;
+        int k=start;
+        while(i<n1 && j<n2){
+            if(left[i]<=right[j]){
+                nums[k]=left[i];
+                i++;
+            }
+            else{
+                nums[k]=right[j];
+                j++;
+            }
+            k++;
+        }
+        
+        //if elements are left in leftSubarray
+        while(i<n1){
+            nums[k]= left[i];
+            i++;
+            k++;
+        }
+        
+        while(j<n2){
+            nums[k]= right[j];
+            j++;
+            k++;
+        }
+        
+    }
+    public static void mergeSort(int[] nums, int start, int end){
+        if(start<end){
+            int mid= start+(end-start)/2;
+            mergeSort(nums,start, mid);
+            mergeSort(nums, mid+1, end);
+            merge(nums, start, mid, end);
+            
+        }
+    }
+    public int[] sortArray(int[] nums) {
+        if(nums==null || nums.length==0) return nums;
+        mergeSort(nums, 0, nums.length-1);
+        return nums;
+    }
+}
