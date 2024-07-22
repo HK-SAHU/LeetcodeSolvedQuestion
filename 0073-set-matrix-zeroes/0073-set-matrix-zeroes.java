ed58a6c6e819be463b7192a60332dc8310dce067
Harsh Kumar Sahu <135067768+HK-SAHU@users.noreply.github.com>
2024-07-22 13:22:49 +0530

    Time: 1 ms (81.13%), Space: 45.5 MB (57.39%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,58 +1,52 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-
-        // Flags to check if the first row and column need to be set to zero
-        boolean firstRowZero = false;
-        boolean firstColZero = false;
-
-        // Check if the first row needs to be set to zero
-        for (int j = 0; j < cols; j++) {
-            if (matrix[0][j] == 0) {
-                firstRowZero = true;
-                break;
-            }
-        }
-
-        // Check if the first column needs to be set to zero
-        for (int i = 0; i < rows; i++) {
-            if (matrix[i][0] == 0) {
-                firstColZero = true;
-                break;
-            }
-        }
-
-        // Use first row and column to mark the zero positions
-        for (int i = 1; i < rows; i++) {
-            for (int j = 1; j < cols; j++) {
-                if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
+        int n =matrix.length;
+        int m= matrix[0].length;
+        // int[] row = new int[n];  --> matrix[..][0]
+        // int[] col= new int[m];   --> matrix[0][..]
+        
+        
+        int col0=1;
+        // step 1: Traverse the matrix and
+        // mark 1st row & col accordingly:
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    if(j!=0){
+                        matrix[0][j]=0;
+                    }
+                    else{
+                        col0=0;
+                    }
                 }
             }
         }
-
-        // Set zeroes based on markers in first row and column
-        for (int i = 1; i < rows; i++) {
-            for (int j = 1; j < cols; j++) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
-                    matrix[i][j] = 0;
+        
+        // Step 2: Mark with 0 from (1,1) to (n-1, m-1):
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[i][j]!=0){
+                    //check for col and row
+                    if(matrix[i][0]==0 || matrix[0][j]==0){
+                        matrix[i][j]=0;
+                    }
                 }
             }
         }
-
-        // Set the first row and column to zero if necessary
-        if (firstRowZero) {
-            for (int j = 0; j < cols; j++) {
-                matrix[0][j] = 0;
+        
+        //step 3: Finally mark the 1st col & then 1st row:
+        if(matrix[0][0]==0){
+            for(int j=0;j<m;j++){
+                matrix[0][j]=0;
             }
         }
-
-        if (firstColZero) {
-            for (int i = 0; i < rows; i++) {
-                matrix[i][0] = 0;
+        
+        if(col0==0){
+            for(int i=0;i<n;i++){
+                matrix[i][0]=0;
             }
         }
+        
     }
 }
