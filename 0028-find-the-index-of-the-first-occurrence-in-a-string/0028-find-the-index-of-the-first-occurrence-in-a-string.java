687ac7e6c62aacce6d1548073e3048702afdb349
Harsh Kumar Sahu <135067768+HK-SAHU@users.noreply.github.com>
2024-07-26 12:23:02 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (9.90%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,25 +1,33 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        int hLen = haystack.length();
-        int nLen = needle.length();
-        int nIndex = 0;
-        for(int i=0; i<hLen; i++){
-            // as long as the characters are equal, increment needleIndex
-            if(haystack.charAt(i)==needle.charAt(nIndex)){
-                nIndex++;
-            }
-            else{
-                // start from the next index of previous start index
-                i=i-nIndex;
-                // needle should start from index 0
-                nIndex=0;
-            }
-            // check if needleIndex reached needle length
-            if(nIndex==nLen){
-                // return the first index
-                return i-nLen+1;
+        // int hLen = haystack.length();
+        // int nLen = needle.length();
+        // int nIndex = 0;
+        // for(int i=0; i<hLen; i++){
+        //     // as long as the characters are equal, increment needleIndex
+        //     if(haystack.charAt(i)==needle.charAt(nIndex)){
+        //         nIndex++;
+        //     }
+        //     else{
+        //         // start from the next index of previous start index
+        //         i=i-nIndex;
+        //         // needle should start from index 0
+        //         nIndex=0;
+        //     }
+        //     // check if needleIndex reached needle length
+        //     if(nIndex==nLen){
+        //         // return the first index
+        //         return i-nLen+1;
+        //     }
+        // }
+        // return -1;
+        
+        for(int i = 0, j = needle.length(); j<=haystack.length(); i++,j++){
+            if(haystack.substring(i,j).equals(needle)){
+                return i;
             }
         }
         return -1;
     }
+
 }
