42414b2da2d67c96458a60c5dd31fe20db547490
Harsh Kumar Sahu <135067768+HK-SAHU@users.noreply.github.com>
2024-07-27 12:20:52 +0530

    Time: 3 ms (98.61%), Space: 43.3 MB (47.38%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,13 +1,40 @@
 class Solution {
     public String simplifyPath(String path) {
-        Stack<String> st= new Stack<>();
-        StringBuilder res= new StringBuilder();   // in this we will be building out string
+        //USING STACK
+        
+//         Stack<String> st= new Stack<>();
+//         StringBuilder res= new StringBuilder();   // in this we will be building our string
+        
+//         String[] p = path.split("/");   // this would split my path by / and would give mt only the directories names
+        
+//         for(int i=0;i<p.length;i++){
+//             if(!st.isEmpty() && p[i].equals("..")){
+//                 st.pop();
+//             }
+            
+//             else if(!p[i].equals("") && !p[i].equals(".") && !p[i].equals("..")){
+//                 st.push(p[i]);
+//             }
+//         }
+        
+//         if(st.isEmpty()) return "/";
+        
+//         while(!st.isEmpty()){
+//             res.insert(0,st.pop()).insert(0,"/");    // as stack do LIFO but i want y answer in FIFO so that's why i did this(i popped and insert at the beginging)
+//         }
+//         return res.toString();
+        
+        
+        // APPROACH 2- USING DEQUE
+        
+        Deque<String> st= new LinkedList<>();
+        StringBuilder res= new StringBuilder();   // in this we will be building our string
         
         String[] p = path.split("/");   // this would split my path by / and would give mt only the directories names
         
         for(int i=0;i<p.length;i++){
             if(!st.isEmpty() && p[i].equals("..")){
-                st.pop();
+                st.poll();
             }
             
             else if(!p[i].equals("") && !p[i].equals(".") && !p[i].equals("..")){
@@ -18,9 +45,10 @@ class Solution {
         if(st.isEmpty()) return "/";
         
         while(!st.isEmpty()){
-            res.insert(0,st.pop()).insert(0,"/");    // as stack do LIFO but i want y answer in FIFO so that's why i did this
+            res.append("/").append(st.pollLast());   // using deque we can achive FIFO easily
         }
         return res.toString();
+        
         
     }
 }
