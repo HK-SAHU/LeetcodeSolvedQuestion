65f0109a8b28546da3b90601c04665382b02679f
Harsh Kumar Sahu <135067768+HK-SAHU@users.noreply.github.com>
2024-07-29 02:20:31 +0530

    Time: 1 ms (100.00%), Space: 41.7 MB (76.32%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.java
+++ 0273-integer-to-english-words/0273-integer-to-english-words.java
@@ -1,115 +1,31 @@
 class Solution {
-    public String numberToWords(int num) {
-        if(num==0){
-            return "Zero";
-        }
-        String[] ones={"","One ","Two ","Three ","Four ","Five ","Six ","Seven ","Eight ","Nine ","Ten ","Eleven ","Twelve ","Thirteen ","Fourteen ","Fifteen ","Sixteen ","Seventeen ","Eighteen ","Nineteen "};
-        String[] tens={"","Twenty ","Thirty ","Forty ","Fifty ","Sixty ","Seventy ","Eighty ","Ninety "};
-        String ans="";
+  public String numberToWords(int num) {
+    return num == 0 ? "Zero" : helper(num);
+  }
 
-        if(num>=1000000000){
-            int temp=num/1000000000;
-            ans+=ones[temp]+"Billion ";
-            num%=1000000000;
-        }
-        if(num>=100000000){
-            int temp=num/1000000;
-                if(temp%100>=0){
-                    ans+=ones[temp/100]+"Hundred ";
-                }
-                temp%=100;
-                int count=0;
-                if(temp>=20){
-                    ans+=tens[temp/10-1];
-                }
-                if(temp<20){
-                    ans+=ones[temp];
-                    count=1;
-                }
-                temp%=10;
-                if(count==0){
-                    if(temp>0){
-                        ans+=ones[temp];
-                    }
-                }
-                ans+="Million ";
-                num%=1000000;
-        }
-        if(num>=10000000){
-            int temp=num/1000000;
-            if(temp>=20){
-                ans+=tens[temp/10-1];
-            }
-            temp%=100;
-            int count=0;
-                if(temp<20){
-                    ans+=ones[temp];
-                    count=1;
-                }
-                temp%=10;
-                if(count==0){
-                    if(temp>0){
-                        ans+=ones[temp];
-                    }
-                }
-            ans+="Million ";
-             num%=1000000;
-        }
-        if(num>=1000000){
-            int temp=num/1000000;
-            ans+=ones[temp]+"Million ";
-            num%=1000000;
-        }
-        if(num>=100000){
-                int temp=num/1000;
-                if(temp>=100){
-                    ans+=ones[temp/100]+"Hundred ";
-                }
-                temp%=100;
-                if(temp>=20){
-                    ans+=tens[temp/10-1];
-                }
-                int count=0;
-                if(temp<20){
-                    ans+=ones[temp];
-                    count=1;
-                }
-                temp%=10;
-                if(count==0){
-                    if(temp>0){
-                        ans+=ones[temp];
-                    }
-                }
-                ans+="Thousand ";
-                num%=1000;
-            }
-        if(num>=10000){
-                int temp=num/1000;
-                if(temp>=20){
-                    ans+=tens[temp/10-1];
-                    temp%=10;
-                }
-                if(temp<20){
-                    ans+=ones[temp];
-                }
-                ans+="Thousand ";
-                num%=1000;
-            }
-        if(num>=1000){
-            ans+=ones[num/1000]+"Thousand ";
-            num%=1000;
-        }
-        if(num>=100){
-            ans+=ones[num/100]+"Hundred ";
-            num%=100;
-        }
-        if(num>=20){
-            ans+=tens[num/10-1];
-            num%=10;
-        }
-        if(num>0 && num<20){
-            ans+=ones[num];
-        }
-        return ans.trim();
-    }
+  private final String[] belowTwenty = {"",        "One",     "Two",       "Three",    "Four",
+                                        "Five",    "Six",     "Seven",     "Eight",    "Nine",
+                                        "Ten",     "Eleven",  "Twelve",    "Thirteen", "Fourteen",
+                                        "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+  private final String[] tens = {"",      "",      "Twenty",  "Thirty", "Forty",
+                                 "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+
+  private String helper(int num) {
+    StringBuilder s = new StringBuilder();
+
+    if (num < 20)
+      s.append(belowTwenty[num]);
+    else if (num < 100)
+      s.append(tens[num / 10]).append(" ").append(belowTwenty[num % 10]);
+    else if (num < 1000)
+      s.append(helper(num / 100)).append(" Hundred ").append(helper(num % 100));
+    else if (num < 1000000)
+      s.append(helper(num / 1000)).append(" Thousand ").append(helper(num % 1000));
+    else if (num < 1000000000)
+      s.append(helper(num / 1000000)).append(" Million ").append(helper(num % 1000000));
+    else
+      s.append(helper(num / 1000000000)).append(" Billion ").append(helper(num % 1000000000));
+
+    return s.toString().trim();
+  }
 }
