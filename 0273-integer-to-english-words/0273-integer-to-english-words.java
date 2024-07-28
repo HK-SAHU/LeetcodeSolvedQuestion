10daeea5299a0415818d8ccbae1e19bb7a35fbf5
Harsh Kumar Sahu <135067768+HK-SAHU@users.noreply.github.com>
2024-07-29 02:18:53 +0530

    Time: 8 ms (55.09%), Space: 42.2 MB (50.81%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.java
+++ 0273-integer-to-english-words/0273-integer-to-english-words.java
@@ -1,32 +1,115 @@
 class Solution {
-    String[] LESS_THAN_20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven",
-    "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen",
-    "Seventeen", "Eighteen", "Nineteen"};
-    String[] TENS = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", 
-    "Eighty", "Ninety"};
-    String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
-    
     public String numberToWords(int num) {
-        if(num == 0)
+        if(num==0){
             return "Zero";
-        int i = 0;
-        String words = "";
-        while(num > 0) {
-            if(num % 1000 != 0)
-                words = getValue(num % 1000) + THOUSANDS[i] + " " + words;
-            num /= 1000;
-            i++;
         }
-        return words.trim();
-    }
-    String getValue(int num) {
-        if(num == 0)
-            return "";
-        else if (num < 20)
-            return LESS_THAN_20[num] + " ";
-        else if (num < 100)
-            return TENS[num / 10] + " " + getValue(num % 10);
-        else
-            return LESS_THAN_20[num / 100] + " Hundred " + getValue(num % 100);
+        String[] ones={"","One ","Two ","Three ","Four ","Five ","Six ","Seven ","Eight ","Nine ","Ten ","Eleven ","Twelve ","Thirteen ","Fourteen ","Fifteen ","Sixteen ","Seventeen ","Eighteen ","Nineteen "};
+        String[] tens={"","Twenty ","Thirty ","Forty ","Fifty ","Sixty ","Seventy ","Eighty ","Ninety "};
+        String ans="";
+
+        if(num>=1000000000){
+            int temp=num/1000000000;
+            ans+=ones[temp]+"Billion ";
+            num%=1000000000;
+        }
+        if(num>=100000000){
+            int temp=num/1000000;
+                if(temp%100>=0){
+                    ans+=ones[temp/100]+"Hundred ";
+                }
+                temp%=100;
+                int count=0;
+                if(temp>=20){
+                    ans+=tens[temp/10-1];
+                }
+                if(temp<20){
+                    ans+=ones[temp];
+                    count=1;
+                }
+                temp%=10;
+                if(count==0){
+                    if(temp>0){
+                        ans+=ones[temp];
+                    }
+                }
+                ans+="Million ";
+                num%=1000000;
+        }
+        if(num>=10000000){
+            int temp=num/1000000;
+            if(temp>=20){
+                ans+=tens[temp/10-1];
+            }
+            temp%=100;
+            int count=0;
+                if(temp<20){
+                    ans+=ones[temp];
+                    count=1;
+                }
+                temp%=10;
+                if(count==0){
+                    if(temp>0){
+                        ans+=ones[temp];
+                    }
+                }
+            ans+="Million ";
+             num%=1000000;
+        }
+        if(num>=1000000){
+            int temp=num/1000000;
+            ans+=ones[temp]+"Million ";
+            num%=1000000;
+        }
+        if(num>=100000){
+                int temp=num/1000;
+                if(temp>=100){
+                    ans+=ones[temp/100]+"Hundred ";
+                }
+                temp%=100;
+                if(temp>=20){
+                    ans+=tens[temp/10-1];
+                }
+                int count=0;
+                if(temp<20){
+                    ans+=ones[temp];
+                    count=1;
+                }
+                temp%=10;
+                if(count==0){
+                    if(temp>0){
+                        ans+=ones[temp];
+                    }
+                }
+                ans+="Thousand ";
+                num%=1000;
+            }
+        if(num>=10000){
+                int temp=num/1000;
+                if(temp>=20){
+                    ans+=tens[temp/10-1];
+                    temp%=10;
+                }
+                if(temp<20){
+                    ans+=ones[temp];
+                }
+                ans+="Thousand ";
+                num%=1000;
+            }
+        if(num>=1000){
+            ans+=ones[num/1000]+"Thousand ";
+            num%=1000;
+        }
+        if(num>=100){
+            ans+=ones[num/100]+"Hundred ";
+            num%=100;
+        }
+        if(num>=20){
+            ans+=tens[num/10-1];
+            num%=10;
+        }
+        if(num>0 && num<20){
+            ans+=ones[num];
+        }
+        return ans.trim();
     }
 }
