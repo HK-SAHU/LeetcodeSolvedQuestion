4f285338615071b947980c2dd0eba1db4d7c27fb
Harsh Kumar Sahu <135067768+HK-SAHU@users.noreply.github.com>
2024-08-07 00:37:16 +0530

    Time: 30 ms (64.45%), Space: 45.3 MB (70.84%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -23,5 +23,22 @@ class Solution {
         }
         
         return sc.reverse().toString();
+        
+        
+//         int count=1;
+//         for(int i =1;i<s.length();i++){
+//             if(s.charAt(i)==s.charAt(i-1)){
+//                 count++;
+//             }
+//             else{
+//                 count=1;  // reset
+//             }
+            
+//             if(count==k){
+//                 String reduced= s.substring(0,i-k+1)+s.substring(i+1);
+//                 return removeDuplicates(reduced,k);
+//             }
+//         }
+//         return s;
     }
 }
