fd954c9f5cf45f0c9a6a2971b16e31646308c10d
Harsh Kumar Sahu <135067768+HK-SAHU@users.noreply.github.com>
2024-08-23 22:35:54 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (5.88%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -15,29 +15,55 @@
  */
 class Solution {
     public void flatten(TreeNode root) {
-        if (root == null) return;
+//         if (root == null) return;
 
-        Stack<TreeNode> stack = new Stack<>();
-        stack.push(root);
+//         Stack<TreeNode> stack = new Stack<>();
+//         stack.push(root);
         
-        TreeNode prev = null;
+//         TreeNode prev = null;
         
-        while (!stack.isEmpty()) {
-            TreeNode node = stack.pop();
+//         while (!stack.isEmpty()) {
+//             TreeNode node = stack.pop();
             
-            if (prev != null) {
-                prev.left = null;
-                prev.right = node;
-            }
+//             if (prev != null) {
+//                 prev.left = null;
+//                 prev.right = node;
+//             }
             
-            if (node.right != null) {
-                stack.push(node.right);
-            }
-            if (node.left != null) {
-                stack.push(node.left);
-            }
+//             if (node.right != null) {
+//                 stack.push(node.right);
+//             }
+//             if (node.left != null) {
+//                 stack.push(node.left);
+//             }
             
-            prev = node;
+//             prev = node;
+//         }
+        
+        //Morris Traversal
+
+        if(root==null)return;
+
+        TreeNode current=root;
+
+        while(current!=null){
+            if(current.left!=null){
+                TreeNode prev=current.left;
+
+                while(prev.right!=null){
+                    prev=prev.right;
+                }
+                prev.right=current.right;
+                current.right=current.left;
+                current.left=null;      //making left nodes null
+            }
+            current=current.right;
         }
+        //another way of making left nodes null
+        // current=root;
+        // while(current!=null){
+        //     current.left=null;
+        //     current=current.right;
+        // }
     }
 }
