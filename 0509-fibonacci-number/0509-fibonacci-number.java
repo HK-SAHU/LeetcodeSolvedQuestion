2e7b21e6c3008273e974771a897d09fc99f94de6
Harsh Kumar Sahu <135067768+HK-SAHU@users.noreply.github.com>
2024-11-10 13:18:40 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (66.22%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,8 +1,14 @@
 class Solution {
     public int fib(int n) {
-        if(n<=1){
-            return n;
-        }
-        return fib(n-1)+fib(n-2);
+        int[] dp= new int[n+1];
+        return f(n, dp);
+    }
+    
+    public int f(int n, int[] dp){
+        if(n<=1) return n;
+        if(dp[n]!=0) return dp[n];
+        int ans= f(n-1, dp)+f(n-2, dp);
+        dp[n]=ans;
+        return ans;
     }
 }
