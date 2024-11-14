b95d08a3a7e7dc7e0e98ce77b056044d3f25bc49
Harsh Kumar Sahu <135067768+HK-SAHU@users.noreply.github.com>
2024-11-14 16:50:58 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (77.53%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -11,18 +11,33 @@ class Solution {
     // }
     ///////////////////// memoization //////////////////////
     
-    public int rob(int[] nums) {
+//     public int rob(int[] nums) {
+//         int n=nums.length;
+//         int[] dp= new int[n];
+//         Arrays.fill(dp,-1);
+//         return takeSkip(nums, 0, dp);
+//     }
+    
+//     public int takeSkip(int[] nums, int i, int[] dp){
+//         if(i>=nums.length) return 0;
+//         if(dp[i]!=-1) return dp[i];
+//         int take= nums[i]+ takeSkip(nums,i+2, dp);
+//         int skip= takeSkip(nums, i+1, dp);
+//         return dp[i]=Math.max(take, skip);
+//     }
+    
+    
+    //////////////////// tabulation ///////////////////////////
+    
+    public int rob(int[] nums){
         int n=nums.length;
         int[] dp= new int[n];
-        Arrays.fill(dp,-1);
-        return takeSkip(nums, 0, dp);
-    }
-    
-    public int takeSkip(int[] nums, int i, int[] dp){
-        if(i>=nums.length) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int take= nums[i]+ takeSkip(nums,i+2, dp);
-        int skip= takeSkip(nums, i+1, dp);
-        return dp[i]=Math.max(take, skip);
+        dp[0]=nums[0];
+        if(n>1) dp[1]= Math.max(nums[0], nums[1]);
+        for(int i=2;i<n;i++){
+            dp[i]= Math.max(nums[i]+ dp[i-2], dp [i-1]);
+        }
+        return dp[n-1];
+        
     }
 }
