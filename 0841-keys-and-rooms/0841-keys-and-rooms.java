fecb9aabd2f4c7633946dd63f5330a9ec8eaf2f9
Harsh Kumar Sahu <135067768+HK-SAHU@users.noreply.github.com>
2024-12-03 17:14:56 +0530

    Time: 0 ms (100.00%), Space: 44 MB (80.19%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,25 +1,50 @@
 class Solution {
+//     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+//         int n= rooms.size();
+//         boolean[] vis = new boolean[n+1];
+//         Queue<Integer> q= new LinkedList<>();
+        
+//         q.add(0);
+//         vis[0]= true;
+//         int visitedRooms=0;
+        
+//         while(!q.isEmpty()){
+//             int front=q.poll();
+//             visitedRooms++;
+            
+//             for(int key: rooms.get(front)){
+//                 if(!vis[key]){
+//                     q.add(key);
+//                     vis[key]= true;
+//                 }
+//             }
+//         }
+//         return visitedRooms==n;
+        
+//     }
+    
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
         int n= rooms.size();
-        boolean[] vis = new boolean[n+1];
-        Queue<Integer> q= new LinkedList<>();
-        
-        q.add(0);
+        boolean[] vis= new boolean[n];
         vis[0]= true;
-        int visitedRooms=0;
-        
-        while(!q.isEmpty()){
-            int front=q.poll();
-            visitedRooms++;
+        dfs(rooms, vis, 0);
+        for(boolean ele:vis){
+            if(ele== false) return false;
             
-            for(int key: rooms.get(front)){
-                if(!vis[key]){
-                    q.add(key);
-                    vis[key]= true;
-                }
+        }
+        
+        return true;
+    }
+    
+    public void dfs(List<List<Integer>> rooms, boolean[] vis, int start){
+        vis[start]= true;
+        
+        for(int ele : rooms.get(start)){
+            if(!vis[ele]){
+                dfs(rooms, vis, ele);
             }
         }
-        return visitedRooms==n;
+        
         
     }
     
