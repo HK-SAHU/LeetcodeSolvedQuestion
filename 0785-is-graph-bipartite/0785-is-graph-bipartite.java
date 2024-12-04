63a26a5956aea5b638b4b02fd468b267091b6bea
Harsh Kumar Sahu <135067768+HK-SAHU@users.noreply.github.com>
2024-12-04 15:16:42 +0530

    Time: 0 ms (100.00%), Space: 45.4 MB (8.83%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,4 +1,42 @@
 class Solution {
+//     public boolean isBipartite(int[][] graph) {
+//         int n = graph.length;
+//         int[] colors = new int[n];
+//         Arrays.fill(colors, -1); // Initialize all nodes with no color (-1)
+
+//         for (int i = 0; i < n; i++) {
+//             if (colors[i] == -1) { // If the node is unvisited
+//                 if (!bfs(i, graph, colors)) { // If any component is not bipartite
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true; // All components are bipartite
+//     }
+
+//     private boolean bfs(int start, int[][] graph, int[] colors) {
+//         Queue<Integer> queue = new LinkedList<>();
+//         queue.add(start);
+//         colors[start] = 0; // Start with color 0
+
+//         while (!queue.isEmpty()) {
+//             int node = queue.poll();
+
+//             for (int neighbor : graph[node]) { // Iterate over neighbors of the node
+//                 if (colors[neighbor] == -1) {
+//                     // Assign opposite color to the neighbor
+//                     colors[neighbor] = 1 - colors[node];
+//                     queue.add(neighbor);
+//                 } else if (colors[neighbor] == colors[node]) {
+//                     // If a neighbor has the same color as the current node, it's not bipartite
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true; // This component is bipartite
+//     }
+    
+    
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
         int[] colors = new int[n];
@@ -6,7 +44,7 @@ class Solution {
 
         for (int i = 0; i < n; i++) {
             if (colors[i] == -1) { // If the node is unvisited
-                if (!bfs(i, graph, colors)) { // If any component is not bipartite
+                if (!dfs(i, graph, colors, 0)) { // Start DFS with color 0
                     return false;
                 }
             }
@@ -14,25 +52,20 @@ class Solution {
         return true; // All components are bipartite
     }
 
-    private boolean bfs(int start, int[][] graph, int[] colors) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
-        colors[start] = 0; // Start with color 0
+    private boolean dfs(int node, int[][] graph, int[] colors, int color) {
+        colors[node] = color; // Color the current node
 
-        while (!queue.isEmpty()) {
-            int node = queue.poll();
-
-            for (int neighbor : graph[node]) { // Iterate over neighbors of the node
-                if (colors[neighbor] == -1) {
-                    // Assign opposite color to the neighbor
-                    colors[neighbor] = 1 - colors[node];
-                    queue.add(neighbor);
-                } else if (colors[neighbor] == colors[node]) {
-                    // If a neighbor has the same color as the current node, it's not bipartite
+        for (int neighbor : graph[node]) { // Iterate over neighbors of the node
+            if (colors[neighbor] == -1) {
+                // If the neighbor is not colored, color it with the opposite color
+                if (!dfs(neighbor, graph, colors, 1 - color)) {
                     return false;
                 }
+            } else if (colors[neighbor] == colors[node]) {
+                // If the neighbor has the same color, the graph is not bipartite
+                return false;
             }
         }
-        return true; // This component is bipartite
+        return true; // No conflicts, this component is bipartite
     }
 }
