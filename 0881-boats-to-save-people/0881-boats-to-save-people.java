e7b0ed765d78ea28e83fcaedbee3d128c22f9707
Harsh Kumar Sahu <135067768+HK-SAHU@users.noreply.github.com>
2024-12-19 19:12:04 +0530

    Time: 3 ms (100.00%), Space: 55.3 MB (21.96%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -1,20 +1,38 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
-        Arrays.sort(people);
-        int boat=0;
-        int j=people.length-1;
-        int i=0;
-        while(i<=j){
-            if(people[i]+people[j]<=limit){
-                i++;
-                j--;
-            }
-            else{
-                j--;
-            }
-            boat++;
+        // Arrays.sort(people);
+        // int boat=0;
+        // int j=people.length-1;
+        // int i=0;
+        // while(i<=j){
+        //     if(people[i]+people[j]<=limit){
+        //         i++;
+        //         j--;
+        //     }
+        //     else{
+        //         j--;
+        //     }
+        //     boat++;
+        // }
+        // return boat;
+        
+        int[] weightFreq = new int[limit + 1];
+        int totalWeight = 0, res = 0;
+        int left = 0, right = weightFreq.length - 1;
+        for (int p: people) {
+            weightFreq[p]++;
         }
-        return boat;
+        while (left <= right) {
+            while(left <= right && weightFreq[right] <= 0) right--;
+            while(left <= right && weightFreq[left] <= 0) left++;
+            if (left > right) break;
+            res++;
+            weightFreq[right]--;
+            if ((right + left) <= limit) {
+                weightFreq[left]--;
+            }
+         }
+        return res;
         
     }
 }
